67bf69d122f7377c487209806070e7e5a90365b4
Tyler Bates <Fondlytyler@gmail.com>
2020-03-23 13:49:31 -0700

    add jquery file and initial script functions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$("h1").click(function() {
+  alert("This is a header.");
+});
+$("p").click(function() {
+  alert("This is a paragraph.");
+});
+$("img").click(function(){
+  alert("This is an image.");
+});
