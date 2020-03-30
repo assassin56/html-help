794f3193d974d8b6b4368d2ee36dc18d81b9c064
Tyler Bates <Fondlytyler@gmail.com>
2020-03-30 14:20:57 -0700

    refactor listener events into a forloop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
-  });
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-  $("img").click(function() {
-    alert("This is an image.");
+  $("div").click(function() {
+    var elements = ["header", "paragraph", "function"];
+    elements.forEach(function(element) {
+      alert('This is ' + element + "!");
+    });
+  //   alert("This is a header.");
+  //   alert("I told you, THIS IS A HEADER!");
+  // });
+  // $("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
+  // $("img").click(function() {
+  //   alert("This is an image.");
   });
 });
