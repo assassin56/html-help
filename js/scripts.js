aa7b7ab5e17e5f07caad3fa5d492e63b81c49618
Tyler Bates <Fondlytyler@gmail.com>
2020-03-30 13:58:06 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
-$("h1").click(function() {
-  alert("This is a header.");
-});
-$("p").click(function() {
-  alert("This is a paragraph.");
-});
-$("img").click(function(){
-  alert("This is an image.");
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  $("img").click(function() {
+    alert("This is an image.");
+  });
 });
